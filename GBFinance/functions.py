852eefe182244982e8d0e4e7688f95f24d412dc2
Hunter22Program <JustinKudela@gmail.com>
2017-03-31 15:55:43 +0000

    Refactored GBFinance functions.py for cleaner code

--- GBFinance/functions.py
+++ GBFinance/functions.py
@@ -1,55 +1,49 @@
 from .models import MonthInc, MonthBal
 
 def report(quarter, year):
-    year = year
+    qmonthstart, qmonthend, qmonthbal, qyearbal = get_quarter_months(quarter, year)
+    quartermonths = MonthInc.objects.filter(date__year=year).filter(
+        date__month__gte=qmonthstart, date__month__lte=qmonthend)
+    creditcards, utilities, loans, savings, surplus = get_quarter_income(quartermonths)
+    networth, loanbalance, savingsbalance = get_quarter_balance(qmonthbal, qyearbal)
+    return (creditcards, utilities, loans, savings, surplus, networth, loanbalance, savingsbalance)
+
+
+def get_quarter_months(quarter, year):
+    '''monthbal is set to month after end of quarter
+    to get the true balances at the end of quarter b/c
+    statements are as of 1st of a month'''
     yearbal = year
     if quarter == "1":
-        monthstart = "1"
-        monthend = "3"
-        monthbal = "4"
+        monthstart, monthend, monthbal = "1", "3", "4"
     elif quarter == "2":
-        monthstart = "4"
-        monthend = "6"
-        monthbal="7"
+        monthstart, monthend, monthbal = "4", "6", "7"
     elif quarter == "3":
-        monthstart = "7"
-        monthend = "9"
-        monthbal = "10"
+        monthstart, monthend, monthbal = "7", "9", "10"
     elif quarter == "4":
-        monthstart = "10"
-        monthend = "12"
-        monthbal = "1"
+        monthstart, monthend, monthbal = "10", "12", "1"
         yearbal = str(int(year)+1)
-    start_date = monthstart
-    end_date = monthend
-    print(start_date, end_date)
-    quartermonths = MonthInc.objects.filter(date__year=year).filter(
-        date__month__gte=monthstart, date__month__lte=monthend)
-    print(quartermonths)
-    creditcards = 0
-    utilities = 0
-    loans = 0
-    savings = 0
-    surplus = 0
+    return (monthstart, monthend, monthbal, yearbal)
+
+
+def get_quarter_income(quartermonths):
+    creditcards, utilities, loans, savings, surplus = 0, 0, 0, 0, 0
     for months in quartermonths:
         creditcards += months.total_personal_creditcards()
         utilities += months.total_utilities()
         loans += months.total_loans()
         savings += months.total_allsavings()
         surplus += months.total_surplus()
+    return (creditcards, utilities, loans, savings, surplus)
+
+
+def get_quarter_balance(qmonthbal, qyearbal):
+    quarter_end = MonthBal.objects.filter(date__year=qyearbal).filter(
+        date__month=qmonthbal)
     try:
-        quarter_end = MonthBal.objects.filter(date__year=yearbal).filter(date__month=monthbal)
         networth = quarter_end[0].networth()
         loanbalance = quarter_end[0].total_loan()
         savingsbalance = quarter_end[0].total_save()
     except IndexError:
-        quarter_end = MonthBal.objects.filter(date__year=yearbal).filter(date__month=monthend)
-        try:
-            networth = quarter_end[0].networth()
-            loanbalance = quarter_end[0].total_loan()
-            savingsbalance = quarter_end[0].total_save()
-        except IndexError:
-            networth = 'N/A'
-            loanbalance = 'N/A'
-            savingsbalance = 'N/A'
-    return (creditcards, utilities, loans, savings, surplus, networth, loanbalance, savingsbalance)
+        networth, loanbalance, savingsbalance = 'N/A', 'N/A', 'N/A'
+    return (networth, loanbalance, savingsbalance)
