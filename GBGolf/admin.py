d43c8113715b72f63967da0455e25bcc7fe8462c
Justin <Justin>
2017-04-02 23:13:21 -0400

    Added putt practice - issues with DB may still exist

--- GBGolf/admin.py
+++ GBGolf/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Course, Round, Shots
+from .models import Course, Round, Shots, PuttPractice
 
 admin.site.register(Course)
 admin.site.register(Round)
 admin.site.register(Shots)
+admin.site.register(PuttPractice)
